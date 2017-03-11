bbf356a6bbfba2615c1f02a8680c3c2b7ff4e2b9
Jean-Francois Chenier <j.f_chenier@hotmail.com>
2017-03-11 17:59:27 -0500

    mod

--- main.c
+++ main.c
@@ -1,81 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef enum {
-    STATE_1,
-    STATE_2,
-    STATE_3,
-    STATE_4,
-    STATE_5,
-    STATE_MAX
-} state_type;
+struct state;
+typedef void state_fn(struct state *);
 
-typedef struct{
-    state_type state;
-    void (*func)(void);
-}state_machine_type;
-
-void run_state(void);
-
-void state1(void);
-void state2(void);
-void state3(void);
-void state4(void);
-void state5(void);
-
-state_machine_type state_machine[] =
+struct state
 {
-    {STATE_1, state1},
-    {STATE_2, state2},
-    {STATE_3, state3},
-    {STATE_4, state4},
-    {STATE_5, state5},
-
+    state_fn * next;
+    int i; // data
 };
 
-state_type next_state = STATE_1;
+state_fn foo, bar;
 
-int main()
+void foo(struct state * state)
 {
-    printf("Hello world!\n");
-    for(;;){
-        run_state();
+    printf("%s %i\n", __func__, ++state->i);
+    state->next = bar;
+}
+
+void bar(struct state * state)
+{
+    printf("%s %i\n", __func__, ++state->i);
+    state->next = foo;
+}
+
+int main(void)
+{
+    struct state state = { foo, 0 };
+    while(state.next){
+        state.next(&state);
     }
-    return 0;
 }
-
-void run_state(void){
-    if(next_state < STATE_MAX){
-        (*state_machine[next_state].func)();
-    }
-    else {
-        printf("error\n\r");
-    }
-}
-
-void state1(void){
-    printf("This is the state 1\n\r");
-    next_state = STATE_2;
-}
-
-void state2(void){
-    printf("This is the state 2\n\r");
-    next_state = STATE_3;
-}
-
-void state3(void){
-    printf("This is the state 3\n\r");
-    next_state = STATE_4;
-}
-
-void state4(void){
-    printf("This is the state 4\n\r");
-    next_state = STATE_5;
-}
-
-void state5(void){
-    printf("This is the state 5\n\r");
-    next_state = STATE_1;
-}
-
-
