d5c2da006bb997ee0b11f1c1478c2bf2e8816bdc
Jean-Francois Chenier <j.f_chenier@hotmail.com>
2017-03-11 18:19:26 -0500

    remove typedef

--- main.c
+++ main.c
@@ -1,31 +1,53 @@
 #include <stdio.h>
 
 struct state;
-typedef void state_fn(struct state *);
+void (*state_fn)(struct state *);
 
 struct state
 {
-    state_fn * next;
+    void (*next)(struct state * state);
     int i; // data
 };
 
-state_fn foo, bar;
+void state1(struct state * state);
+void state2(struct state * state);
+void state3(struct state * state);
+void state4(struct state * state);
+void state5(struct state * state);
 
-void foo(struct state * state)
+void state1(struct state * state)
 {
-    printf("%s %i\n", __func__, ++state->i);
-    state->next = bar;
+    printf("this is the state 1\n\r");
+    state->next = state2;
 }
 
-void bar(struct state * state)
+void state2(struct state * state)
 {
-    printf("%s %i\n", __func__, ++state->i);
-    state->next = foo;
+    printf("this is the state 2\n\r");
+    state->next = state3;
+}
+
+void state3(struct state * state)
+{
+    printf("this is the state 3\n\r");
+    state->next = state4;
+}
+
+void state4(struct state * state)
+{
+    printf("this is the state 4\n\r");
+    state->next = state5;
+}
+
+void state5(struct state * state)
+{
+    printf("this is the state 5\n\r");
+    state->next = state1;
 }
 
 int main(void)
 {
-    struct state state = { foo, 0 };
+    struct state state = { state1, 0 };
     while(state.next){
         state.next(&state);
     }
