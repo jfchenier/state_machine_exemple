2e9d5d313864a82e478bd54da364ae75e7365959
Jean-Francois Chenier <j.f_chenier@hotmail.com>
2017-03-11 21:20:25 -0500

    cleanup

--- main.c
+++ main.c
@@ -4,17 +4,15 @@
 
 struct state
 {
-    void (*next)(struct state * state);
-    int i; // data
+    void (*next)(struct state *state);
+    void *arg;
 };
 
-void (*state_function)(struct state *);
-
-void state1(struct state * state);
-void state2(struct state * state);
-void state3(struct state * state);
-void state4(struct state * state);
-void state5(struct state * state);
+void state1(struct state *state);
+void state2(struct state *state);
+void state3(struct state *state);
+void state4(struct state *state);
+void state5(struct state *state);
 
 int main(void)
 {
@@ -24,32 +22,34 @@ int main(void)
     }
 }
 
-void state1(struct state * state)
+void state1(struct state *state)
 {
+    int i = 5;
     printf("this is the state 1\n\r");
+    state->arg = 5;
     state->next = state2;
 }
 
-void state2(struct state * state)
+void state2(struct state *state)
 {
     printf("this is the state 2\n\r");
     state->next = state3;
 }
 
-void state3(struct state * state)
+void state3(struct state *state)
 {
     printf("this is the state 3\n\r");
     state->next = state4;
 }
 
-void state4(struct state * state)
+void state4(struct state *state)
 {
     printf("this is the state 4\n\r");
     state->next = state5;
 }
 
-void state5(struct state * state)
+void state5(struct state *state)
 {
     printf("this is the state 5\n\r");
-    state->next = state1;
+    state->next = 0;
 }
